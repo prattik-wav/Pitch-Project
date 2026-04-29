a538af553aa7836213182729fc0e0c34fa9478de
prattik-wav <prattik.music666@gmail.com>
2026-04-29 22:12:00 +0530

    Implement FastAPI server

---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+import sys
+import os
+
+# Point Python to compiled C++ engine inside the build directory
+sys.path.append(os.path.join(os.path.dirname(__file__), "build"))
+import handcricket_ai
+
+# Intialize FastAPI server
+app = FastAPI(title="Pitch.io", version="1.0")
+
+# Health check Endpoint
+@app.get("/")
+def read_root():
+    return {"message": "FastAPI server is running", "c++ engine": "Connected"}
+
+# Prediction Endpoint
+@app.get("/predict")
+def get_prediction(ai_is_batting: bool, difficulty: int):
+    # mock datas for initial test
+    # Endpoint to pull live game arrays from MySQL
+    mock_memory_cache = [4, 4, 1, 4, 6]
+
+    # Call the C++ engine
+    ai_choice = handcricket_ai.get_ai_prediction(mock_memory_cache, ai_is_batting, difficulty)
+
+    return {
+        "status": "success",
+        "scenario": "AI batting" if ai_is_batting else "AI bowling",
+        "difficulty_level": difficulty,
+        "player_memory_analyzed": mock_memory_cache,
+        "ai_prediction": ai_choice
+    }
